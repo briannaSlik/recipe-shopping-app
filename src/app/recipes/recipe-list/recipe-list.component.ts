fecb4be9c26883272cb702e7d38032df60bb4823
BriAnna Slik <daffadelicious@gmail.com>
2024-01-17 17:10:51 -0700

    created model for recipe, worked on recipe list and recipe detail components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('Vietnamese Pho', 'A delicious soup broth with glass noodles, a choice of meat, and a plethora of vegetables', 'https://images.rawpixel.com/image_800/cHJpdmF0ZS9zdGF0aWMvaW1hZ2Uvd2Vic2l0ZS8yMDIyLTA0L2xyL3B4NzA5NzU5LWltYWdlLWt3dnY2eW5lLmpwZw.jpg')
+  ];
 }
