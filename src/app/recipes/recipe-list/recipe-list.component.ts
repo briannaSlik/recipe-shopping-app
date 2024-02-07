ace694641e0dfaee23db222e65d2dc835fc86442
BriAnna Slik <daffadelicious@gmail.com>
2024-02-06 18:07:15 -0700

    added services for recipes and shopping list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,19 +1,19 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('Vietnamese Pho', 'A delicious soup broth with glass noodles, a choice of meat, and a plethora of vegetables', 'https://images.rawpixel.com/image_800/cHJpdmF0ZS9zdGF0aWMvaW1hZ2Uvd2Vic2l0ZS8yMDIyLTA0L2xyL3B4NzA5NzU5LWltYWdlLWt3dnY2eW5lLmpwZw.jpg')
-  ];
+export class RecipeListComponent implements OnInit{
+  recipes: Recipe[] = [];
 
-  onRecipeSelected(recipe: Recipe){
-    this.recipeWasSelected.emit(recipe)
+  constructor(private recipeService: RecipeService){}
+
+  ngOnInit(){
+    this.recipes = this.recipeService.getRecipes();
   }
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,34 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+
+@Injectable()
+export class RecipeService{
+    public recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe('Vietnamese Pho', 
+        'A delicious soup broth with glass noodles, a choice of meat, and a plethora of vegetables', 
+        'https://images.rawpixel.com/image_800/cHJpdmF0ZS9zdGF0aWMvaW1hZ2Uvd2Vic2l0ZS8yMDIyLTA0L2xyL3B4NzA5NzU5LWltYWdlLWt3dnY2eW5lLmpwZw.jpg',
+        [
+            new Ingredient('Broth', 1),
+            new Ingredient('Glass Noodles', 1),
+            new Ingredient('Sliced Beef', 1),
+            new Ingredient('Green Onions', 1),
+            new Ingredient('Onion', 1),
+            new Ingredient('Cilantro', 1),
+            new Ingredient('Lime', 1)
+        ])
+    ];
+
+    constructor(private slService: ShoppingListService){}
+
+    getRecipes(){
+        return this.recipes.slice();
+    }
+
+    addToShoppingList(ingredients: Ingredient[]){
+        this.slService.addIngredients(ingredients);
+    }
+}
