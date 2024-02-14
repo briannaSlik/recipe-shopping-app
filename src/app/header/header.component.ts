14082c0d5a99cbc111aa3a5e3f06fd58822d6ecb
BriAnna Slik <daffadelicious@gmail.com>
2024-02-14 00:33:42 -0700

    set up routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,13 +7,5 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent {
   collapsed = true;
-  @Output() featureSelected = new EventEmitter<string>();
-
-  constructor(){
-
-  }
-
-  onSelect(feature: string){
-    this.featureSelected.emit(feature);
-  }
+  constructor(){}
 }
