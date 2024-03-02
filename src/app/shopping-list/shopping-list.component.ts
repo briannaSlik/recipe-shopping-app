6f695134c90b2102f7b313be812a82c6143b8c6e
BriAnna Slik <daffadelicious@gmail.com>
2024-03-02 16:42:05 -0700

    added form functionality

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -15,6 +15,10 @@ export class ShoppingListComponent implements OnInit, OnDestroy{
 
   constructor(private slService: ShoppingListService){}
 
+  onEditItem(index: number){
+    this.slService.startedEditing.next(index);
+  }
+
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
     this.subscription = this.slService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
