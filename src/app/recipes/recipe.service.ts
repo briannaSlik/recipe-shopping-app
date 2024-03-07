74a01a51984a082a77e9e0693cab02011f0a0c68
BriAnna Slik <daffadelicious@gmail.com>
2024-03-07 13:18:08 -0700

    added http request functionality

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,22 +8,29 @@ import { Subject } from "rxjs";
 export class RecipeService{
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe('Vietnamese Pho', 
-        'A delicious soup broth with glass noodles, a choice of meat, and a plethora of vegetables', 
-        'https://images.rawpixel.com/image_800/cHJpdmF0ZS9zdGF0aWMvaW1hZ2Uvd2Vic2l0ZS8yMDIyLTA0L2xyL3B4NzA5NzU5LWltYWdlLWt3dnY2eW5lLmpwZw.jpg',
-        [
-            new Ingredient('Broth', 1),
-            new Ingredient('Glass Noodles', 1),
-            new Ingredient('Sliced Beef', 1),
-            new Ingredient('Green Onions', 1),
-            new Ingredient('Onion', 1),
-            new Ingredient('Cilantro', 1),
-            new Ingredient('Lime', 1)
-        ])
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe('Vietnamese Pho', 
+    //     'A delicious soup broth with glass noodles, a choice of meat, and a plethora of vegetables', 
+    //     'https://images.rawpixel.com/image_800/cHJpdmF0ZS9zdGF0aWMvaW1hZ2Uvd2Vic2l0ZS8yMDIyLTA0L2xyL3B4NzA5NzU5LWltYWdlLWt3dnY2eW5lLmpwZw.jpg',
+    //     [
+    //         new Ingredient('Broth', 1),
+    //         new Ingredient('Glass Noodles', 1),
+    //         new Ingredient('Sliced Beef', 1),
+    //         new Ingredient('Green Onions', 1),
+    //         new Ingredient('Onion', 1),
+    //         new Ingredient('Cilantro', 1),
+    //         new Ingredient('Lime', 1)
+    //     ])
+    // ];
+
+    private recipes: Recipe[] = [];
 
     constructor(private slService: ShoppingListService){}
+
+    setRecipes(recipes: Recipe[]){
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice())
+    }
 
     getRecipes(){
         return this.recipes.slice();
